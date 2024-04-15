e4e660e4db068ad49f0acf965310c610c0a7e293
pranit-harekar <pranit.harekar@e-hps.com>
2024-04-15 17:33:52 -0400

    Adds analytics skeleton

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 
+import { AnalyticsSkeleton } from '@/app/ui/components/dashboard/analytics/AnalyticsSkeleton'
 import { lusitana } from '@/app/ui/fonts'
 
 export const metadata: Metadata = {
@@ -10,6 +11,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Analytics</h1>
+      <AnalyticsSkeleton />
     </main>
   )
 }
