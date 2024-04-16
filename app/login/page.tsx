69c30f8f22fa581514e1d1d39aeebc9cfbc477ab
pranit-harekar <pranit.harekar@e-hps.com>
2024-04-15 22:11:39 -0400

    Fix broken logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,11 +11,9 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <WIPCallout />
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <Link href="/">
-              <Logo />
-            </Link>
-          </div>
+          <Link href="/">
+            <Logo />
+          </Link>
         </div>
         <LoginForm />
       </div>
